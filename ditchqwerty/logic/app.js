379cd65d7a3fbf15dba8b9ba5730fbd04b0fac06
Nicko <nwmariano@gmail.com>
2020-08-24 17:29:09 -0400

    lots of fiddling with the layout select menu. It is now horizontal and fades out on the left and right. No functionality yet

--- ditchqwerty/logic/app.js
+++ ditchqwerty/logic/app.js
@@ -98,7 +98,7 @@ console.log(window.location.href );
 // this is the true init, which is only called once. Init will have to be renamed
 // Call to initialize
 function start() {
-	// document.querySelector('#layoutName').innerHTML = currentLayout;
+	//document.querySelector('#layoutName').innerHTML = currentLayout;
 	document.querySelector('.cheatsheet').innerHTML = keyboardDivs;
 	inputKeyboard.innerHTML = customLayout;
 	// scoreMax = wordLimitModeInput.value;
